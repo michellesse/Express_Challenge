dbc8c6ae6e3627b370652443c95e0e4bc8bb3467
Brenda Michelle <bmichelle.psic@gmail.com>
2019-05-16 12:04:35 -0500

    ejercicio1

--- index.js
+++ index.js
@@ -10,17 +10,24 @@ app.get('/bye', function (req, res) {
     res.send('Bye bye!');
   });
 
-  //app.use(express.static(C:\Users\BM\Desktop\Weman\Semana_5\Express_Challenge\assets + '/assets'));
-//app.use(express.static('assets'));
-
-//app.use('/gatitos', express.static('assets));
-
-//app.use('/static', express.static(path.join(__dirname, 'public')))
-
 //app.use('/assets', express.static('assets')) // de este modo lee el archivo si está en el mismo directorio que tu index.
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
-  
 
+//Despues del método (app.use) viene el path ('/assets')
+//ver routing en pagina de express, hay varias opciones de cómo matchear la url. 
+
+//dos parptes fundamentales en las funcioens request (cosas relacionadas con la peticion) response (lo que nos sirve para responderle al usuario) Cualqueir metodo (get, post) va necesitar metodo, accion y 
+app.get ('/algo', (req, res) => {
+    res.status(404).send("error 404");
+})
+
+app.post ('/otroalgo', (req, res) => {
+    res.status(403).send("Otroalgo")
+})
+
+app.patch ('/unomas', (req, res) => {
+    res.status(265).send("Y este es el último :D")
+})
 
 app.listen(3000, function () {
         console.log('Corriendo :)');
